5a57d08826f4679b35dc7098854db3abbeca068a
ponywen <pony076152340@gmail.com>
2023-07-13 18:30:00 +0800

    Split loading component

--- app/(hasNav)/(pt)/profile/[handle]/loading.jsx
+++ app/(hasNav)/(pt)/profile/[handle]/loading.jsx
@@ -1,28 +1,8 @@
-import "./profile.css"
+import { Loading } from '@/components/loading';
+
 
 const ProfileLoading = ()=>{
-    return(
-        <div className="w-full h-48">
-            <div className="mx-auto w-fit h-full flex flex-col justify-center">
-                <div className="lds-spinner">
-                    <div/>
-                    <div/>
-                    <div/>
-                    <div/>
-                    <div/>
-                    <div/>
-                    <div/>
-                    <div/>
-                    <div/>
-                    <div/>
-                    <div/>
-                    <div/>
-                </div>
-                <p className="w-fit text-center"></p>
-            </div>
-        </div>
-        
-    );
+    return(<Loading/>);
 }
 
 
---
+++ components/loading.jsx
@@ -0,0 +1,28 @@
+import './loading.css'
+
+export const Loading = ()=>{
+    return(
+        <div className="w-full h-48">
+            <div className="mx-auto w-fit h-full flex flex-col justify-center">
+                <div className="lds-spinner">
+                    <div/>
+                    <div/>
+                    <div/>
+                    <div/>
+                    <div/>
+                    <div/>
+                    <div/>
+                    <div/>
+                    <div/>
+                    <div/>
+                    <div/>
+                    <div/>
+                </div>
+                <p className="w-fit text-center"></p>
+            </div>
+        </div>
+        
+    );
+}
+
+
