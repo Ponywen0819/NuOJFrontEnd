1cfa1c6b29346d0fde00509473310d6c04dfa85a
ponywen <pony076152340@gmail.com>
2023-07-16 16:20:48 +0800

    Add loading and not-found handling to Problem page

---
+++ app/(hasNav)/(pt)/problem/[id]/not-found.jsx
@@ -0,0 +1,10 @@
+const ProblemNotFound = ()=>{
+    return(
+        <div className="text-center mx-auto h-48 flex flex-col justify-center">
+            <p className="p-2 w-fit mx-auto text-2xl font-mono bg-orange-400 rounded-lg">{"404 Not Found :("}</p>
+        </div>
+        
+    )
+}
+
+export default ProblemNotFound;
--- app/(hasNav)/(pt)/problem/[id]/page.jsx
+++ app/(hasNav)/(pt)/problem/[id]/page.jsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useState, useEffect } from 'react';
+import { Loading } from '@/components/loading';
+import NotFound from './not-found';
 import { HOST } from '@/setting';
 
 const ProblemDetail = ({ params })=>{
     let id = params.id;
-    const [data, setData] = useState(null);
+    const [data, setData] = useState({status: 0});
+
     useEffect(()=>{
         getInfo()
     },[]);
@@ -24,7 +27,10 @@ const ProblemDetail = ({ params })=>{
                 "tl": json.data.setting.time_limit,
                 "ml": json.data.setting.memory_limit
             }
-            setData(info);
+            setData({...info, status:1});
+        }
+        else{
+            setData({status: -1});
         }
     }
 
@@ -36,35 +42,43 @@ const ProblemDetail = ({ params })=>{
         {key: "note", lable: '備註'}
     ]
 
-    return(
-        <div className="py-5">
-            <div className="w-3/4 mx-auto h-fit rounded border-2 shadow flex flex-col p-10 gap-10 bg-white mb-5">
-                <div className="text-center">
-                    <p id="title" className="text-4xl font-medium my-2"> {data?.title} </p>
-                    <p id="TL-text" className="text-lg font-medium my-2"> 程式運行時間限制（TL）：{data?.tl} 秒</p>
-                    <p id="ML-text" className="text-lg font-medium my-2"> 程式運行時間限制（ML）：{data?.ml} MB</p>
+    switch (data.status){
+        case 1:
+            return(
+                <div className="py-5">
+                    <div className="w-3/4 mx-auto h-fit rounded border-2 shadow flex flex-col p-10 gap-10 bg-white mb-5">
+                        <div className="text-center">
+                            <p id="title" className="text-4xl font-medium my-2"> {data?.title} </p>
+                            <p id="TL-text" className="text-lg font-medium my-2"> 程式運行時間限制（TL）：{data?.tl} 秒</p>
+                            <p id="ML-text" className="text-lg font-medium my-2"> 程式運行時間限制（ML）：{data?.ml} MB</p>
+                        </div>
+                        {
+                            lines.map((line)=>{
+                                return(
+                                    <div key={`${line.key}`}>
+                                        <p className="text-xl font-semibold my-5">{line.lable}</p>
+                                        {
+                                            data?.[line.key].split("\n").map((l,index)=>(
+                                                <p key={`${line.key}:${index}`} class="py-1"> {l} </p>
+                                            ))
+                                        }
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="w-3/4 mx-auto border-2 text-lg flex flex-col gap-10 p-10 shadow bg-white">
+                        <textarea id="code_area" className="resize-none w-full h-20"></textarea>
+                        <button className="bg-blue-700 w-full delay-50 p-2 hover:bg-blue-500 rounded text-white text-2xl"> 提交 </button>
+                    </div>
                 </div>
-                {
-                    lines.map((line)=>{
-                        return(
-                            <div key={`${line.key}`}>
-                                <p className="text-xl font-semibold my-5">{line.lable}</p>
-                                {
-                                    data?.[line.key].split("\n").map((l,index)=>(
-                                        <p key={`${line.key}:${index}`} class="py-1"> {l} </p>
-                                    ))
-                                }
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <div className="w-3/4 mx-auto border-2 text-lg flex flex-col gap-10 p-10 shadow bg-white">
-                <textarea id="code_area" className="resize-none w-full h-20"></textarea>
-                <button className="bg-blue-700 w-full delay-50 p-2 hover:bg-blue-500 rounded text-white text-2xl"> 提交 </button>
-            </div>
-        </div>
-    )
+            )
+        case -1:
+            return <NotFound></NotFound>
+        default:
+            return <Loading/>               
+
+    }
 }
 
 export default ProblemDetail;
